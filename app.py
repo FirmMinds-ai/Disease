349401c58c345370f66c03f6fe04ec480aed9910
FirmMinds-ai <87647533+FirmMinds-ai@users.noreply.github.com>
2021-09-24 19:38:43 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -7,14 +7,29 @@ from flask import jsonify
 # initiate flask
 app = Flask(__name__)
 
+o2 =  {'Obesity_Type_I': 0,
+  'Obesity_Type_III': 1,
+  'Obesity_Type_II': 2,
+  'Overweight_Level_I': 3,
+  'Overweight_Level_II': 4,
+  'Normal_Weight': 5,
+  'Insufficient_Weight': 6}
+
+
 
 with open('std.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 
-with open('lr.pkl', 'rb') as file:
-    lr = pickle.load(file)
+with open('lir.pkl', 'rb') as file:
+    lir = pickle.load(file)
 
+with open('std2.pkl', 'rb') as file:
+    scaler2 = pickle.load(file)
+
+
+with open('rfc2.pkl', 'rb') as file:
+    rf2 = pickle.load(file)
 
 @app.route('/pred1/',methods = ['POST'])
 def home() :
@@ -28,9 +43,27 @@ def home() :
             Inp.append(int(x))
         X = scaler.transform([Inp])
 
-        out = lr.predict(X)[0]
+        out = lir.predict(X)[0]
 
-        d = {"Prediction" : int(out)}
+        d = {"Prediction" : out*100}
+
+    return jsonify(d)
+
+@app.route('/pred2/',methods = ['POST'])
+def home1() :
+
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        l = data["data"]
+        Inp = []
+        for x in l :
+            Inp.append(int(x))
+        X = scaler2.transform([Inp])
+
+        out = rf2.predict(X)[0]
+
+        d = {"Prediction" : list(o2.keys())[out]}
 
     return jsonify(d)
 
